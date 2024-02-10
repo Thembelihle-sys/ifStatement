2c56bd1698cb8cff51bdc63cbe23a8022b521a25
Thembelihle-sys <141932567+Thembelihle-sys@users.noreply.github.com>
2024-02-10 23:12:17 +0200

    Add files via upload

---
+++ ifStatements.py
@@ -0,0 +1,11 @@
+num1= int(input('Enter the first number: '))
+num2= int(input('Enter the second number: '))
+
+# Choose the larger number
+if num1 > num2:
+     larger_num = num1
+else:
+     larger_num = num2
+
+     print('The larger number is:', larger_num)
+
